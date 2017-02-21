1e239faec21b5a222e672e778e49a632baaf0573
turboc1208 <chip.cox@coxdogs.com>
2017-02-21 14:32:36 -0600

    updated build_entity_list

--- calalarm.py
+++ calalarm.py
@@ -87,7 +87,7 @@ class calalarm(appapi.AppDaemon):
 
     self.readalarmstate()
       # setup listeners
-    boolean_list=self.buildentitylist("group.alarm_clocks")
+    boolean_list=self.build_entity_list("group.alarm_clocks",['input_boolean'])
     self.log("boolean_list={}".format(boolean_list))
     for switch in boolean_list:
       teststate=self.listen_state(self.input_boolean_changed, switch)
@@ -312,8 +312,9 @@ class calalarm(appapi.AppDaemon):
   def print_calendar(self):
     self.log("Printing Calendar")
     for room in self.alarms:
-      if not self.alarms[room]["handle"]=="":
+      if not self.alarms[room]["handle"]==None:
         ainfo=self.info_timer(self.alarms[room]["handle"])
+        self.log("room={},alarmstate active?={}, handle {}".format(room, self.alarmstate[room]["active"],ainfo))
         self.log("{:<10}{:<10}{}".format(room,self.alarmstate[room]["active"],ainfo["info_time"]))
 
   def log(self,msg,level="INFO"):
@@ -340,13 +341,49 @@ class calalarm(appapi.AppDaemon):
       dateformat="%Y-%m-%dT%H:%M:%SZ"
     return dateformat
 
-  def buildentitylist(self,ingroup):
-    self.log("ingroup={}".format(ingroup))
+  ######################
+  #
+  # build_entity_list (self, ingroup, inlist - optional: defaults to all entity types))
+  #
+  # build a list of all of the entities in a group or nested hierarchy of groups
+  #
+  # ingroup = Starting group to cascade through
+  # inlist = a list of the entity types the list may contain.  Use this if you only want a list of lights and switches for example.
+  #            this would then exclude any input_booleans, input_sliders, media_players, sensors, etc. - defaults to all entity types.
+  #
+  # returns a python list containing all the entities found that match the device types in inlist.
+  ######################
+  def build_entity_list(self,ingroup,inlist=['all']):
     retlist=[]
-    devtyp, devname = self.split_entity(ingroup)
-    if devtyp=="group":
-      for entity in self.get_state(ingroup,attribute="all")["attributes"]["entity_id"]:
-        retlist.append(self.buildentitylist(entity))
+    types=[]
+    typelist=[]
+
+    # validate values passed in
+    if not self.entity_exists(ingroup):
+      self.log("entity {} does not exist in home assistant".format(ingroup))
+      return None
+    if isinstance(inlist,list):
+      typelist=inlist
     else:
-      retlist=ingroup
+      self.log("inlist must be a list ['light','switch','media_player'] for example")
+      return None
+
+    # determine what types of HA entities to return
+    if "all" in typelist:
+      types=["all"]
+    else:
+      types= types + typelist
+      types.append("group")            # include group so that it doesn't ignore child groups
+
+    # check the device type to see if it is something we care about
+    devtyp, devname = self.split_entity(ingroup)
+    if (devtyp in types) or ("all" in types):                # do we have a valid HA entity type
+      if devtyp=="group":                                    # entity is a group so iterate through it recursing back into this function.
+        for entity in self.get_state(ingroup,attribute="all")["attributes"]["entity_id"]:
+          newitem=self.build_entity_list(entity,typelist)    # recurse through each member of the child group we are in.
+          if not newitem==None:                              # None means there was a problem with the value passed in, so don't include it in our output list
+            retlist.extend(newitem)                          # all is good so concatenate our lists together
+      else:
+        retlist.append(ingroup)                                      # actual entity so return it as part of a list so it can be concatenated
     return retlist
+
